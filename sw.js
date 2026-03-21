97b8e95b054d5a120abb1581d246e7a0d1901788
Ruslan591 <rus3212@gmail.com>
2026-03-21 22:10:50 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,38 @@
-const CACHE_NAME = 'modelRating-cache-v1';
+const CACHE_NAME = 'modelRating-cache-v2';
+
 const urlsToCache = [
   './',
   './index.html',
+  './forecast.html',
+  './current_weather.html',
   './manifest.json',
-  './a_digital_vector_icon_features_a_white_minimalist.png',
-  './style.css'
+  './verify.css',
+  './current_weather.css',
+  './a_digital_vector_icon_features_a_white_minimalist.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
