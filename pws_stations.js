5c9d4ac5691647dd3697d7e33ad298962c547c6f
Ruslan591 <rus3212@gmail.com>
2026-05-10 22:01:30 +0300

    Add files via upload

--- pws_stations.js
+++ pws_stations.js
@@ -6,15 +6,15 @@
    offset = +1.5
 ========================================================= */
 var PWS_SYNC_STATIONS = [
-    { id: "IODESA137", name: "пос. Котовского",  pressureOffset: 0    },
+    { id: "IODESA137", name: "пос. Котовского",  pressureOffset: -5.3  },
     { id: "IODESA138", name: "Центр",             pressureOffset: 10.3 },
-    { id: "IODESA139", name: "Чудо Город",        pressureOffset: 0    },
-    { id: "IODESS41",  name: "Судостроительная",  pressureOffset: 0    },
+    { id: "IODESA139", name: "Чудо Город",        pressureOffset: 1.2  },
+    { id: "IODESS41",  name: "Судостроительная",  pressureOffset: 4.2  },
     { id: "IODESS44",  name: "Аркадия",           pressureOffset: -1.8 },
     { id: "IODESS16",  name: "Таирова",           pressureOffset: 1.2  },
-    { id: "IODESS31",  name: "Савиньон",          pressureOffset: 0    },
+    { id: "IODESS31",  name: "Савиньон",          pressureOffset: 17.9 },
     { id: "IODESS37",  name: "Застава",           pressureOffset: 8.2  },
-    { id: "IKRASN91",  name: "пос. Степовое",     pressureOffset: 0    },
+    { id: "IKRASN91",  name: "пос. Степовое",     pressureOffset: -1.5 },
 ];
 /* Публичные API ключи Weather Underground */
 var PWS_WU_KEYS = [
