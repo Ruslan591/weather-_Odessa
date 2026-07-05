4fc835d916439e7dd6958659c1b37d8dd6c98522
ruslan591 <@email.com>
2026-07-05 12:28:51 +0300

    calibrate_pws_pressure: офсеты давления по SYNOP/BUFR

--- pws_stations.js
+++ pws_stations.js
@@ -11,7 +11,7 @@ var PWS_SYNC_STATIONS = [
     { id: "IODESA139", name: "Чудо Город",  pressureOffset: 1.2 },
     { id: "IODESS41", name: "Судостроительная",  pressureOffset: 4.2 },
     { id: "IODESS44", name: "Аркадия",  pressureOffset: -1.7 },
-    { id: "IODESS16", name: "Таирова",  pressureOffset: 0.7 },
+    { id: "IODESS16", name: "Таирова",  pressureOffset: 0.6 },
     { id: "IODESS31", name: "Савиньон",  pressureOffset: 18.6 },
     { id: "IODESS37", name: "Застава",  pressureOffset: 2.2 },
     { id: "IKRASN91", name: "пос. Степовое",  pressureOffset: -1.1 },
