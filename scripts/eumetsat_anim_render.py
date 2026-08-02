c6645a74d039a7d88dae51c7dbe9f252d52156a7
Ruslan591 <rus3212@gmail.com>
2026-08-02 16:08:28 +0300

    Add VIS 0.6 HRFI (mtg_fd:vis06_hrfi) to LAYERS

--- scripts/eumetsat_anim_render.py
+++ scripts/eumetsat_anim_render.py
@@ -112,6 +112,7 @@ LAYERS = {
     "li_afa":     {"name": "mtg_fd:li_afa",          "style": "",                                   "step_minutes": 5},
     "geocolour":  {"name": "mtg_fd:rgb_geocolour",   "style": "",                                   "step_minutes": 10},
     "ir108":      {"name": "mtg_fd:ir105_hrfi",      "style": "mtg_fd:mtg_fd_ir105_hrfi_grayscale",  "step_minutes": 10},
+    "vis06":      {"name": "mtg_fd:vis06_hrfi",      "style": "",                                   "step_minutes": 10},
     "cloudtype":  {"name": "mtg_fd:rgb_cloudtype",   "style": "raster",                              "step_minutes": 10},
     "cloudphase": {"name": "mtg_fd:rgb_cloudphase",  "style": "raster",                              "step_minutes": 10},
     "fog":        {"name": "mtg_fd:rgb_fog",         "style": "raster",                              "step_minutes": 10},
